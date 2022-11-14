1b013a99c486b952906d407e4a8d98012c4a1f11
taimoorbaig99 <muhammadtaimoorbaig64@gmail.com>
2022-11-14 12:00:33 +0400

    Javascript20chpass

---
+++ app.js
@@ -0,0 +1,303 @@
+// =====================CHP-1========================
+
+// alert("Error! Please enter a valid password");
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+
+
+// alert("Hello... I can run JS through my web browser's console");
+
+// var  myname= ("Taimoor baig");
+// var myage=("16 years old");
+// var mymessage=("Hello World");
+
+// alert(myname);
+// alert(myage);
+// alert(mymessage);
+
+
+// var text=("PIZZA\nPIZZ\nPIZ\nPI\nP");
+// alert(text);
+
+
+// var email=("My email address is muhammadtaimoorbaig64@gmail.com");
+// alert(email);
+
+// var a=("I am trying to learn from the Book A Smarter Way To Learn Javascript");
+// alert(a);
+
+//  var design=("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+//  window.alert (design);
+
+
+// alert("Error! please enter a valid password") 
+//         let = prompt("please enter a password " ,"Guest")
+//         console.log('password');
+//         alert("Welocme to js land... \n Happy coding")
+
+//         let = prompt("happy coding","creating this page from creating additional dialog")
+//         // console.log('')
+//         alert("Hello...I can run JS through my browser's console")
+
+// =====================CHP-2========================
+
+//  var username;
+// var myname = "TAIMOOR";
+//  alert(myname)
+//  var a = "16";
+//  var b = "year old";
+//  alert(a+ b);
+//  alert("Mobile app development");
+//  var food =("PIZZA\nPIZZ\nPIZ\nPI\nP");
+//  alert(food);
+//  var email = ("My email address is @example.com");
+//  alert(email);
+//  var message =("I am trying to learn from the Book A smater \n to learn JavaScript");
+//  alert(message);
+// var a = ("Yeah! I can write HTML content through JavaScript")
+// document.write(a)
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+//==============================CHP-3==================================
+
+//  var a ="I am 16";
+//  var b ="year old";
+//  alert(a+ b);
+//  var a = ("You have visit 12 times ");
+//  alert(a);
+//  var age = '16'
+//  var ans = prompt(" I am ____ year old")
+//  if(ans === '17'){
+//     document.write("MY birth year is 2005")
+//  }
+ 
+//  var a =prompt("visitor's name");
+//  var b =prompt("product title");
+//  var c =prompt("quality");
+//  document.write(a+b+c);
+
+//  ==============================CHP-4==================================
+
+//  var abc (legal)
+ // var ABC (legal)
+ // var 123 (illegal)
+ // var 1ab (illegal)
+ // var ab1 (legal)
+ // var $abc (legal)
+ // var ab$c (legal)
+ // var ab-c  (illegal)
+ // var ab_c  (legal)
+ // var ab.c  (illegal)
+ // var abc.  (illegal)
+
+//  var a = ("1. Declare 3 variables in one statement. ");
+//   alert(a);
+//   var b = ("2. Declare 5 legal & 5 illegal variable names.");
+//   alert(b);
+//   var c =("3. Display this in your browser ");
+//   alert(c);
+//   var d =("a) A heading stating “Rules for naming JS variables”");
+//   alert(d);
+//   var a = ("b) Variable names can only contain ' _____' ,'_______' and '_________'.")
+//   var ans =prompt("b) Variable names can only contain_____, _______and ,_________.")
+  
+// var head = "<h2>Rules for naming JS variables</h2>";
+// document.write ("<br>" + head);
+// var variableName = "Variable names can only contain Numbers, $ and _. For example $my_1stVariable";
+// document.write ("<br>" + variableName);
+// var variableName1 = "Variables must begin with a Letter, $ or_. For example $name, _name or name";
+// document.write ("<br>" + variableName1);
+// var variableName2 = "Variable names are case sensitive";
+// document.write ("<br>" + variableName2);
+// var variableName3 = "Variable names should not be JS keywords";
+// document.write ("<br>" + variableName3);
+
+//=============================CHP-5===================================
+
+//var a=3
+//var b=5
+//document.write(a+b);
+// var a=3
+// var b=5
+// document.write(a-b);
+// var a=4
+// var b=5
+// document.write(a*b);
+// var a=3
+// var b=5
+// document.write(a/b);
+// var a=3
+// var b=5
+// document.write(a%b);
+// var x = 10;
+// var a = x + 5;
+// document.write(a)
+// var a = 5;
+// var b = a++;
+// document.write(b);    
+// document.write("<br>"+a);
+// var a=600 
+// var b=5
+// document.write(a*b);   
+// var  table = 4;
+// var length = 10;
+// var i = 1
+
+//=======================CHP-6==============================
+
+//========= CHP-6 / 9 MATH EXPRESSIONS =========
+
+// var a = 10;
+// var b = ++a;
+// var b = a++;
+// var b = --a;
+// var b = a--;
+// document.write(b);
+
+// var a = 2;
+// var b = 1;
+// var c = --a - --b + ++b + b--;
+// document.write(c);
+
+//================= CHP-9 / 11 USER INPUT & CONDITIONAL STATEMENT====================
+
+// var city = prompt("Enter your city")
+// if (city == "karachi"){
+//     alert("Welcome to Ned UNI")
+// }
+
+// var gender = prompt("Enter your gender")
+// if (gender == "male"){
+//     alert("Good Morning sur")
+// }
+// if (gender == "female"){
+//     alert("good morning mam")
+    
+// }
+
+// var signal = prompt("Enter signal color")
+// if (signal == "red"){
+//     alert("Must Stop")
+// }
+// if (signal == "red"){
+//     alert("Ready to move")
+    
+// }
+// if (signal == "pink"){
+//     alert("move now")
+    
+// }
+
+// var fuel = +prompt("enter your car's fuel")
+// if (fuel == 0.2) {
+//     alert("Please refill the fuel in your car")
+// }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+
+
+
+
+// var num = prompt("guess the secret number")
+// if (num == 7){
+//     alert("Bingo! Correct answer")
+// }
+// if (num == 8){
+//     alert("Close enough to the correct answer")
+// }
+
+// var divisible = +prompt("Enter your value")
+// if (divisible == 6 , 9 , 12 , 15 , 18 ,21 ){
+//     alert("yes its divisible by 3")
+// }
+    
+// var num = +prompt ("Enter Your value")
+// if (num == 2 , 4 , 8 , 10 , 12 , 14 , 16 ,18 ,20){
+//     alert("even number")
+// }
+// if (num ===1 , 3 , 5 , 7 , 9 , 11 , 13 , 15 ,17 , 19 , 21){
+//     alert("odd number")
+// }
+
+
+
+//=================== CHP-12 /13 ; IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS ==================
+
+
+// var num = prompt("Enter a number")
+// if (num == "0"){
+//     alert("number is zero")
+// }
+// if (num > "0"){
+//     alert("number is positive")
+// }
+// if (num < "0"){
+//     alert("number is negative")
+// }
+
+// function isVowel(argument){
+
+// 	var text;
+// 	var argument = argument.toLowerCase();
+
+//     var vowels = (['a', 'e', 'i', 'o', 'u']);
+
+//     for (var i = 0; i <= vowels.length; i++){
+//         if (argument != vowels[i]) {
+//             continue;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
+
+// var char = "A";
+
+// if (isVowel(char)) {
+// 	alert(char + " is a vowel");
+// } else {
+// 	alert(char + " is not a vowel");
+// }
+
+
+
+//====================CHP-14 / 16============================
+
+
+
+
+// var arr = [];
+
+// var objectA = {}
+
+// var arr = ["LaLa"];
+
+// var arr = [123 , 456 , 789];
+
+// var arr = [123 , "Sahar" , "Byworld"];
+
+
+
+// var arr = ["Taimoor" , "Sahar" , "Ahsan "]
+// var arr2 = [320 , 230 , 480]
+
+
+
+// var score = [320,230,480,1200]
+// var oscore =[120,230,320,480]
+
+// var city = ["Lahore" , "Islamabad", "","Multan","Faislabad"]
+// console.log(city.slice(2,4))
+
+
+
